d6bf44a2dac417941680d8aa86c9f292710d3886
Franco Lesme <101482668+LesmeFranco@users.noreply.github.com>
2025-05-08 16:09:37 -0300

    Update base.js

--- actividades/ej_01/scripts/base.js
+++ actividades/ej_01/scripts/base.js
@@ -3,4 +3,5 @@ document.addEventListener('DOMContentLoaded', () => {
     const fechaActual = new Date(document.lastModified);
     const opcionesFecha = { year: 'numeric', month: 'long', day: 'numeric' };
     fechaElemento.textContent = fechaActual.toLocaleDateString('es-ES', opcionesFecha);
+    document.getElementById('fecha').textContent = new Date().toLocaleDateString('es-AR');
 });
