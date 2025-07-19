0bd30d95b30a745acb22a39e354b46a5795dc8bc
Garcia Louzan Ignacio <96312981+iglouzan@users.noreply.github.com>
2025-07-18 23:05:59 -0300

    Delete actividades/ej_03/styles/scripts directory

--- actividades/ej_03/styles/scripts/base.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const fechaElemento = document.getElementById('fecha');
-    const fechaActual = new Date(document.lastModified);
-    const opcionesFecha = { year: 'numeric', month: 'long', day: 'numeric' };
-    fechaElemento.textContent = fechaActual.toLocaleDateString('es-ES', opcionesFecha);
-});
